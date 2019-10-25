115be665c39871a25095a46912692cec4bf980bb
Alex Dragan <adragan029@gmail.com>
2019-10-25 12:53:56 +0300

    Revert "Create README.md"
    This reverts commit 96c4a64b39e514e8656063d7d53e32171cc6d0e5.

--- run.bat
+++ run.bat
@@ -1,46 +1,26 @@
 @echo off
-
 setlocal ENABLEDELAYEDEXPANSION
-set LANG=%1
-set NUM_RUNS=%2
-set PROJ_PATH=%3
-set EXE_PATH=%PROJ_PATH%
+SET LEN=%1
+SET LANG=%2
+SET "EXE_PATH=%3"
 
-if %LANG%==-help (
-	echo "1st arg -> language(-j, -cpp)"
-	echo "2nd arg -> num runs"
-	echo "3rd arg -> project path"
-	echo "ex: run.bat -j 30 sample_project/"
-	goto :end
-)
-
-set argC=0
-for %%x in (%*) do (
-	set /A argC+=1
-)
-
-if not !argC! equ 3 (
-	echo Invalid number of args.
-	goto :end
-)
-
-if %LANG%==-j (
-	call %PROJ_PATH%\gradlew -b %PROJ_PATH%\build.gradle jar
-	for /F %%i in ('where /r %PROJ_PATH%\build\libs *.jar') DO (
-		set EXE_PATH=%%i
+IF "%LANG%" == "-c" (
+	SET IS_EXE="0"
+	FOR /F %%i in ('where /r %EXE_PATH%\bin *.exe') DO (
+		SET IS_EXE="1"						
+		FOR /L %%A IN (1,1,%LEN%) DO (
+			call %%i
+		)
 	)
+)					
+			
+IF "%LANG%" == "-j" (	
+	call %EXE_PATH%\gradlew -b %EXE_PATH%\build.gradle jar
+	
+	FOR /F %%i in ('where /r %EXE_PATH%\build\libs *.jar') DO (
+		SET IS_EXE="1"						
+		FOR /L %%A IN (1,1,%LEN%) DO (
+			call %%i
+		)
+	)	
 )
-
-if %LANG%==-cpp (
-	rem call vcvarsall x64
-	rem call msbuild %PROJ_PATH%
-	for /F %%i in ('where /r %PROJ_PATH%\bin *.exe') DO (
-		set EXE_PATH=%%i
-	)
-)
-
-for /L %%i IN (1,1,%NUM_RUNS%) do (
-	call !EXE_PATH!
-)
-
-:end
